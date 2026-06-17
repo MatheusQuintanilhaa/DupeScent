494663a618db8173046f88539e910bb45da7f534
Matheus Quintanilha <matheussantos.quintanilha@gmail.com>
2026-06-17 00:15:55 -0300

    fixing

--- src/pages/PerfumesPage.jsx
+++ src/pages/PerfumesPage.jsx
@@ -2,7 +2,6 @@
 // Página reutilizável para Masculinos e Femininos
 
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Card from "../components/Card";
